84cb43602cd181480fdfaba3ae3889005995770d
Rafael G <rafygonzalez089@gmail.com>
2022-11-20 17:15:56 -0500

    [Add] Find Nearest Neightboard. Add Capability to eat more fruits

--- agent.py
+++ agent.py
@@ -1,8 +1,12 @@
+import collections
+import operator
+import numpy as np
+from sklearn.neighbors import KDTree
 import torch
 import random
 import numpy as np
 from collections import deque
-from game import SnakeGameAI, Direction, Point
+from game import FOOD_NUM, SnakeGameAI, Direction, Point, index_of
 from model import Linear_QNet, QTrainer
 from helper import plot
 
@@ -10,16 +14,106 @@ MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
 LR = 0.001
 
+NNRecord = collections.namedtuple("NNRecord", ["point", "distance"])
+NNRecord.__doc__ = """
+Used to keep track of the current best guess during a nearest
+neighbor search.
+"""
+
+BT = collections.namedtuple("BT", ["value", "left", "right"])
+BT.__doc__ = """
+A Binary Tree (BT) with a node value, and left- and
+right-subtrees.
+"""
+
+class GameFood:
+    def __init__(self, xl, xr, yl, yr):
+        self.xl = xl
+        self.xr = xr
+        self.yl = yl
+        self.yr = yr
+
+def SED(X, Y):
+    return sum((i-j)**2 for i, j in zip(X, Y))
+
+
+def kdtree(points):
+    """Construct a k-d tree from an iterable of points.
+
+    This algorithm is taken from Wikipedia. For more details,
+
+    > https://en.wikipedia.org/wiki/K-d_tree#Construction
+
+    """
+    k = len(points[0])
+
+    def build(*, points, depth):
+        """Build a k-d tree from a set of points at a given
+        depth.
+        """
+        if len(points) == 0:
+            return None
+
+        points.sort(key=operator.itemgetter(depth % k))
+        middle = len(points) // 2
+
+        return BT(
+                value = points[middle],
+                left = build(
+                        points=points[:middle],
+                        depth=depth+1,
+                    ),
+                right = build(
+                        points=points[middle+1:],
+                        depth=depth+1,
+                    ),
+        )
+
+    return build(points=list(points), depth=0)
+
+def find_nearest_neighbor(*, tree, point):
+            """Find the nearest neighbor in a k-d tree for a given
+            point.
+            """
+            k = len(point)
+
+            best = None
+            def search(*, tree, depth):
+                """Recursively search through the k-d tree to find the
+                nearest neighbor.
+                """
+                nonlocal best
+
+                if tree is None:
+                    return
+
+                distance = SED(tree.value, point)
+                if best is None or distance < best.distance:
+                    best = NNRecord(point=tree.value, distance=distance)
+
+                axis = depth % k
+                diff = point[axis] - tree.value[axis]
+                if diff <= 0:
+                    close, away = tree.left, tree.right
+                else:
+                    close, away = tree.right, tree.left
+
+                search(tree=close, depth=depth+1)
+                if diff**2 < best.distance:
+                    search(tree=away, depth=depth+1)
+
+            search(tree=tree, depth=0)
+            return best.point
+
 class Agent:
 
     def __init__(self):
         self.n_games = 0
-        self.epsilon = 0 # randomness
-        self.gamma = 0.9 # discount rate
-        self.memory = deque(maxlen=MAX_MEMORY) # popleft()
+        self.epsilon = 0  # randomness
+        self.gamma = 0.9  # discount rate
+        self.memory = deque(maxlen=MAX_MEMORY)  # popleft()
         self.model = Linear_QNet(11, 256, 3)
         self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
-
 
     def get_state(self, game):
         head = game.snake[0]
@@ -27,11 +121,20 @@ class Agent:
         point_r = Point(head.x + 20, head.y)
         point_u = Point(head.x, head.y - 20)
         point_d = Point(head.x, head.y + 20)
-        
+
         dir_l = game.direction == Direction.LEFT
         dir_r = game.direction == Direction.RIGHT
         dir_u = game.direction == Direction.UP
         dir_d = game.direction == Direction.DOWN
+
+        reference_points = []
+        for food in game.food:
+            reference_points.append((food.x, food.y))
+
+
+        tree = kdtree(reference_points)
+        point = find_nearest_neighbor(tree=tree, point=(game.snake[0].x,  game.snake[0].y))
+        target = index_of(point, game.food)
 
         state = [
             # Danger straight
@@ -57,12 +160,12 @@ class Agent:
             dir_r,
             dir_u,
             dir_d,
-            
-            # Food location 
-            game.food.x < game.head.x,  # food left
-            game.food.x > game.head.x,  # food right
-            game.food.y < game.head.y,  # food up
-            game.food.y > game.head.y  # food down
+
+            # Game Food Target
+            game.food[target].x < game.head.x,  # food left
+            game.food[target].x > game.head.x,  # food right
+            game.food[target].y < game.head.y,  # food up
+            game.food[target].y > game.head.y  # food down
             ]
 
         return np.array(state, dtype=int)
@@ -117,7 +220,7 @@ def train():
         # perform move and get new state
         reward, done, score = game.play_step(final_move)
         state_new = agent.get_state(game)
-
+        
         # train short memory
         agent.train_short_memory(state_old, final_move, reward, state_new, done)
 
@@ -135,7 +238,7 @@ def train():
                 agent.model.save()
 
             print('Game', agent.n_games, 'Score', score, 'Record:', record)
-
+    
             plot_scores.append(score)
             total_score += score
             mean_score = total_score / agent.n_games
--- game.py
+++ game.py
@@ -3,10 +3,18 @@ import random
 from enum import Enum
 from collections import namedtuple
 import numpy as np
+from timeit import default_timer as timer
 
 pygame.init()
 font = pygame.font.Font('arial.ttf', 25)
-#font = pygame.font.SysFont('arial', 25)
+# font = pygame.font.SysFont('arial', 25)
+
+def index_of(val, in_list):
+    try:
+        return in_list.index(val)
+    except ValueError:
+        return -1 
+
 
 class Direction(Enum):
     RIGHT = 1
@@ -14,29 +22,36 @@ class Direction(Enum):
     UP = 3
     DOWN = 4
 
+
 Point = namedtuple('Point', 'x, y')
 
 # rgb colors
 WHITE = (255, 255, 255)
-RED = (200,0,0)
+RED = (200, 0, 0)
 BLUE1 = (0, 0, 255)
 BLUE2 = (0, 100, 255)
-BLACK = (0,0,0)
+BLACK = (0, 0, 0)
 
 BLOCK_SIZE = 20
-SPEED = 40
+SPEED = 80
+
+MAX_TIME = 2
+FOOD_NUM = 5
 
 class SnakeGameAI:
 
     def __init__(self, w=640, h=480):
+        self.start = timer()
+        self.end = 0
         self.w = w
         self.h = h
+        self.eaten = []
+        self.food_num = FOOD_NUM
         # init display
         self.display = pygame.display.set_mode((self.w, self.h))
         pygame.display.set_caption('Snake')
         self.clock = pygame.time.Clock()
         self.reset()
-
 
     def reset(self):
         # init game state
@@ -46,21 +61,35 @@ class SnakeGameAI:
         self.snake = [self.head,
                       Point(self.head.x-BLOCK_SIZE, self.head.y),
                       Point(self.head.x-(2*BLOCK_SIZE), self.head.y)]
+        
 
         self.score = 0
         self.food = None
         self._place_food()
         self.frame_iteration = 0
 
-
     def _place_food(self):
-        x = random.randint(0, (self.w-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE
-        y = random.randint(0, (self.h-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE
-        self.food = Point(x, y)
-        if self.food in self.snake:
-            self._place_food()
+
+        ##if self.food: 
+        ##    return
+    
+        self.food = []
+        self.eaten = []
+
+        for _ in range(self.food_num):
+            x = random.randint(0, (self.w-BLOCK_SIZE)//BLOCK_SIZE)*BLOCK_SIZE
+            y = random.randint(0, (self.h-BLOCK_SIZE)//BLOCK_SIZE)*BLOCK_SIZE
+            food = Point(x, y)
+            self.food.append(food)
 
 
+        for food in self.food:
+            if food in self.snake:
+                self._place_food()
+
+
+            
+     
     def play_step(self, action):
         self.frame_iteration += 1
         # 1. collect user input
@@ -76,19 +105,43 @@ class SnakeGameAI:
         # 3. check if game over
         reward = 0
         game_over = False
-        if self.is_collision() or self.frame_iteration > 100*len(self.snake):
+
+        iterationException = self.frame_iteration > 100*len(self.snake)
+        collide = self.is_collision()
+        currentTime = timer()
+        timeExceeded = currentTime - self.start > MAX_TIME
+
+
+        if collide or iterationException or timeExceeded:
+            reward = -15
             game_over = True
-            reward = -10
+            self.start = timer()
             return reward, game_over, self.score
 
+
         # 4. place new food or just move
-        if self.head == self.food:
-            self.score += 1
-            reward = 10
+
+        shouldDelete = True
+        for food in self.food:
+              if self.head == food:
+                 exists = index_of(food, self.eaten)
+                 if exists == -1:
+                    self.score += 1
+                    reward = 15
+                    shouldDelete = False
+                    self.eaten.append(food)
+                    self.food.remove(food)
+                    self.start = timer()
+                    
+
+        if shouldDelete:
+             self.snake.pop()
+
+        if len(self.eaten) == FOOD_NUM:
+            self.end = timer()
             self._place_food()
-        else:
-            self.snake.pop()
-        
+      
+
         # 5. update ui and clock
         self._update_ui()
         self.clock.tick(SPEED)
@@ -116,10 +169,19 @@ class SnakeGameAI:
             pygame.draw.rect(self.display, BLUE1, pygame.Rect(pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE))
             pygame.draw.rect(self.display, BLUE2, pygame.Rect(pt.x+4, pt.y+4, 12, 12))
 
-        pygame.draw.rect(self.display, RED, pygame.Rect(self.food.x, self.food.y, BLOCK_SIZE, BLOCK_SIZE))
+        for food in self.food:
+            exists = index_of(food, self.eaten)
+            if exists == -1:
+                pygame.draw.rect(self.display, RED, pygame.Rect(food.x, food.y, BLOCK_SIZE, BLOCK_SIZE))
 
+
+
+    
         text = font.render("Score: " + str(self.score), True, WHITE)
         self.display.blit(text, [0, 0])
+
+        record = font.render("Time Elapse: " + str(int(timer() - self.start)) , True, WHITE)
+        self.display.blit(record, [0, 30])
         pygame.display.flip()
 
 
